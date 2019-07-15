79d1c78d6a2e9761742b431668c4f333d7366355
Jenish Mangukiya <34957550+jenishmangukiya@users.noreply.github.com>
2019-07-15 18:34:48 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,12 +4,14 @@ const https = require('https');
 
 bot.on('ready',()=>{
 	console.log("Working & Online");
+	
 });
 
 bot.on('message',msg=>{
-  if(msg.content === "Hello"){
+	if(msg.content === "Hello"){
 		msg.reply("Hello!");
 	}
+	setTimeout(msg.reply("1"),20000);
 });
 
 bot.login(process.env.BOT_TOKEN);
