5ec1958c279a9d1d4ac54529e23b3e05c42b58fa
Jenish Mangukiya <34957550+jenishmangukiya@users.noreply.github.com>
2019-07-16 00:51:09 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,8 +5,17 @@ const https = require('https')
 client.on('ready',()=>{
 	console.log("Working & Online")
 	setInterval(function() {
-			var data='';
-    		var options = 'https://www.bitmex.com/api/v1/trade?symbol=XBTUSD&side=buy&count=500&start=0&reverse=true';
+		var data='';
+		
+		var d = new Date();
+			
+		d.setMilliseconds(000);
+		var endD=d.toISOString();
+			
+		d.setSeconds(d.getSeconds()-2);
+		var startD=d.toISOString();
+		
+    		var options = 'https://www.bitmex.com/api/v1/trade?symbol=XBTUSD&count=500&start=1&reverse=false&startTime='+startD+'&endTime='+endD;
     		var request = https.request(options, function (res) {
 				res.on('data', function (chunk) {
 					data += chunk;
@@ -14,14 +23,27 @@ client.on('ready',()=>{
 				res.on('end', function () {
 					//var generalChannel = client.channels.get("579297644178833424") //QD
 					var generalChannel = client.channels.get("600017052366929921")
-					//generalChannel.send(data)
+					
 					var dArr = JSON.parse(data);
+					var x=0;
+					var sz=0;
+					var c=0;
 					for(i=0;i<dArr.length;i++)
 					{
-						if(dArr[i]['size']>=1000)
+						if(dArr[i]['side']=='Buy' && (dArr[i]['price']>=10000))
 						{
-							generalChannel.send(" :flag_ao: " + " [" + dArr[i]['timestamp'] +"] BitMEX "+ dArr[i]['symbol'] +" **"+ dArr[i]['size'] +"** contracts market bought at **" + dArr[i]['price'] + "**")
+							x+=dArr[i]['price']
+							sz+=dArr[i]['size']
+							c++
 						}
+					}
+					if(sz>=2000000)
+					{
+						console.log(x/c);
+						console.log(sz);
+						
+						generalChannel.send(" :flag_in: " + " [" + dArr[i]['timestamp'] +"] BitMEX XBTUSD **"+ sz +"** contracts market bought at **" + x/c + "**")
+						
 					}
 				});
     		});
@@ -29,7 +51,7 @@ client.on('ready',()=>{
         		console.log(e.message)
     		});
     		request.end()  
-  	},5000);
+  	},2000);
 });
 
 client.on('message',msg=>{
